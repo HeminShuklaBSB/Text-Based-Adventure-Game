07e4a6f170e46c9c61d912d2431ab56fe201689f
HeminShuklaBSB <106305400+HeminShuklaBSB@users.noreply.github.com>
2023-05-14 02:24:50 +0300

    Add files via upload
    Added Backpack and Health Bar

---
+++ start.py
@@ -0,0 +1,64 @@
+#Notes
+#If possible please keep the updateBackpack() before the updateHealth(), it just looks better, although I am working on having them "spawn in" if you will in better spots
+
+
+#Imports
+from visuals import healthBar, BackPack
+import time
+import os
+
+#Defining functions for convinience 
+#Just making things faster to call
+def cls():
+    os.system('cls')
+
+def sleep(num=1): 
+    #So if you just call sleep() with no parameters it will stop 1 second as the default, but you can also specify more if you want
+    time.sleep(num)
+
+#Initialising Values
+backpack = BackPack
+inputs = ["use", "take", "attack", "run", "ask", "tell", "look", "drop", "north", "south", "east", "west", "yes", "no"] #List of valid inputs
+health = healthBar
+
+
+#General Functions
+def validateInput(inp): 
+    #I suspect that this will have to be edited soon, but until then it's the idea that counts, you get it
+    while inp not in inputs:
+        if inp != "":
+            print ("I'm sorry, I didn't quite understand that.")
+            print ("Please try again")
+        inp = input("> ")
+    return inp
+
+#Press Start
+input("Press Enter To Start: ")
+cls()
+try:
+    #All the Actual Code
+    health.updateHealthBar(100)
+    backpack.updateBackPack()
+    print ("Introduction")
+    inp = validateInput(input("> "))
+    backpack.add("Burger")
+    backpack.add("Dagger")
+    backpack.updateBackPack()
+    health.updateHealthBar(25)
+    health.updateHealthBar(75, user=False)
+    inp = validateInput(input("> "))
+except Exception as e:
+    print ("Sorry there's been an error, we'll restart")
+    print (f"Error: {e}")
+    input()
+finally:
+    #I need to keep this here
+    #updateHealthBar(health) will not run unless there it is called afterwards again 
+    #(and there is an input at some point in front of it)
+    #This should allow it to keep being called as many times as I need 
+    #It should also only run when the script is over or when there is an error
+    sleep()
+    cls()
+    backpack.updateBackPack(finish=True)
+    health.updateHealthBar(input("Press Enter To Finish The Game: "), finish=True)
+    
---
+++ visuals.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+
+def doNothing():
+    #Literally does nothing but please don't delete it 
+    #It is used so that the user can't click exit 
+    #Or alt f4 out of any of the widgets
+    pass
+
+def initialiseWindow(window, title, size): #Just made a function because it's used at least twice, might as well save some lines
+    window.title(title)
+    window.configure(background="black")
+    window.minsize(size[0], size[1])
+    window.maxsize(size[0], size[1])
+    window.protocol("WM_DELETE_WINDOW", doNothing)
+
+#Health Bar Section
+class healthBar:
+    healthBarWindow = tk.Tk() #Initialising the healthBarWindow
+    healthBarWindow.protocol("WM_DELETE_WINDOW", doNothing)
+    userHealth = 100
+
+    def __init__(self):
+        pass
+
+    def updateHealthBar(health, user=True, finish=False): #Updates the visual health bar, for both the player and (if needed) the enemy who is represented in green
+        if finish:
+            exit()
+        
+        healthBar.healthBarWindow.destroy()
+        healthBar.healthBarWindow = tk.Tk()
+        if user:
+            windowHeight = 100
+            healthBar.userHealth = health
+        else:
+            windowHeight = 170
+
+        initialiseWindow(healthBar.healthBarWindow, "Health Bar", (500, windowHeight))
+        healthBarMain = tk.Canvas(healthBar.healthBarWindow, width=500, height=windowHeight, bg="black")
+        healthBarMain.create_rectangle(50, 30, 450, 70, fill="gray")
+        healthBarMain.create_rectangle(50, 30, 50 + healthBar.userHealth * 4, 70, fill="red")
+        if not user:
+            healthBarMain.create_rectangle(50, 100, 450, 140, fill="gray")
+            healthBarMain.create_rectangle(50, 100, 50 + health * 4, 140, fill="green")
+
+        healthBarMain.pack()
+        
+
+
+
+#Backpack Section
+class BackPack:
+    backpack = []
+    backpackWindow = tk.Tk()
+    backpackWindow.protocol("WM_DELETE_WINDOW", doNothing) #Stops the user from being able to alt f4 or click exit on the widget to prevent errors
+    def __init__(self):
+        pass
+    
+    def updateBackPack(finish=False): #Updates the visual list of what the backpack contains
+        if finish: #Does nothing if called at the end
+            pass
+        else:
+            BackPack.backpackWindow.destroy()
+            BackPack.backpackWindow = tk.Tk()
+            initialiseWindow(BackPack.backpackWindow, "Backpack Contents", (200, 800))
+            tk.Label(BackPack.backpackWindow, anchor="center", text="Items:", bd=0, bg="black", fg="lightGray", font="MS 30").pack()
+            if len(BackPack.backpack) == 0:
+                tk.Label(BackPack.backpackWindow, anchor="w", text="You Have No Items To Display!", bd=0, bg="black", fg="lightGray", font="MS 10").pack()
+            else:
+                for i in range(len(BackPack.backpack)):
+                    tk.Label(BackPack.backpackWindow, anchor="w", text=f"- {BackPack.backpack[i]}", bd=0, bg="black", fg="lightGray", font="MS 20").pack()
+            
+
+    def add(item):
+        BackPack.backpack.append(item) #Adds an item to the backpack
+    
+    def remove(item):
+        try:
+            BackPack.backpack.remove(item) #Removes an item from the backpack
+        except Exception as e:
+            print ("Sorry, there was an error, we're going to have to restart") #To make debugging easier/looks better if it doesn't just crash 
+            print (f"Error: {e}")
+
+
