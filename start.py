f08e7b4151403c5270d997e71849bc468b2818b7
Meepy11 <benjamin.dyas@bsbonline.net>
2023-05-29 12:53:11 +0300

    Added Class selection and started on story

--- start.py
+++ start.py
@@ -18,7 +18,7 @@ def sleep(num=1):
 
 #Initialising Values
 backpack = BackPack
-basicInputs = ["use", "take", "attack", "run", "ask", "tell", "look", "drop", "north", "south", "east", "west", "yes", "no"] #List of valid inputs
+basicInputs = ["use", "take", "attack", "run", "ask", "tell", "look", "drop", "n", "s", "e", "w", "yes", "no"] #List of valid inputs
 health = healthBar
 
 
@@ -33,25 +33,70 @@ def validateInput(inp, inputs=basicInputs):
     return inp
 
 def stonehill():
-    print("Welcome to the Stonehill Inn, the heart of the small town of Phandalin. This inn serves as a reststop for travellers making the long journey towards the Sword Mountains. \n Nestled in the foothills of the Sword Mountains, Phandalin is a quaint little town, home to around 1000 people. It sits at the base of Icespire Peak, which is one of the highest peaks of the mountains. Atop of the summit sits the feared White Dragon of Icespire, who has overrun the Icespire Fort and periodically flies down to attack the village.")
-    print("The Stonehill Inn has a rather modest interior, with wooden pillars haphazardly strewn across the floor, holding up the floor above, which holds the bedrooms. The innkeeper calls over to you and asks")
-
+    cls()
+    print("Welcome to the Stonehill Inn, the heart of the small town of Phandalin. This inn serves as a reststop for travellers making the long journey towards the Sword Mountains. \nNestled in the foothills of the Sword Mountains, Phandalin is a quaint little town, home to around 1000 people. It sits at the base of Icespire Peak, which is one of the highest peaks of the mountains. Atop of the summit sits the feared White Dragon of Icespire, who has overrun the Icespire Fort and periodically flies down to attack the village. \n ")
+    input("Press enter to continue: ")
+    cls()
+    print("The Stonehill Inn has a rather modest interior, with wooden pillars haphazardly strewn across the floor, holding up the floor above, which holds the bedrooms. The innkeeper calls over to you and asks, \n'Oi! You! You look like a traveller. You heard of that Harvin Wester in town? He's the townmaster, and I heard he's looking for an adventurer to do some quests for him. Heard he's paying out big time!'")
+    input("Press enter to continue: ")
+    cls()
+    print("What would you like to do? Explore the upstairs of Stonehill Inn? (up), Go out the door to the main square to the south (s)")
+    inp = ""
+    validateInput()
+    if inp == "up":
+        stonehillUp()
+    elif inp == "s":
+        phandalinSquare()
+    else:
+        print 
 classList = ["Druid","Ranger","Barbarian","Paladin",]
+
+classStats = [["Class","Race","Health","Weapon Name","AttackMin","AttackMax","Defense","Multiplier","Speed"],
+              ["Druid","Elf",10,"Scimitar",1,6,13,1,35],
+              ["Ranger","Elf",10,"Longbow",1,8,14,3,25],
+              ["Barbarian","Elf",14,"Greataxe",1,12,14,2,15],
+              ["Paladin","Elf",10,"Longsword",1,8,18,2,15],
+              ]
 
 #Press Start
 input("Press Enter To Start: ")
 cls()
 try:
     #All the Actual Code
+    user = character()
     health.updateHealthBar(100)
     backpack.updateBackPack()
     classChoice = ""
     while classChoice not in classList:
+        cls()
         print("Class Options:")
         for i in classList:
             print(">",i)
         classChoice = input("Please select a class: ")
+        classChoice = classChoice.title()
+
+    cls()
     print("You have selected the",classChoice,"class!")
+    row = 0
+    if classChoice == "Druid":
+        row = 1
+    elif classChoice == "Ranger":
+        row = 2
+    elif classChoice == "Barbarian":
+        row = 3
+    elif classChoice == "Paladin":
+        row = 4
+    user.health = classStats[row][2]
+    weaponName = classStats[row][3]
+    user.attackMin = classStats[row][4]
+    user.attackMax = classStats[row][5]
+    user.defense = classStats[row][6]
+    user.multiplier = classStats[row][7]
+    user.speed = classStats[row][8]
+    print("Your stats are: \nHealth:",user.health,"\nWeapon Name:",weaponName,"\nAttack: between",user.attackMin,"and",user.attackMax,"\nDefense:",user.defense,"\nSpeed:",user.speed)
+    input("Press enter to continue: ")
+
+    stonehill()
 
 except Exception as e:
     print ("Sorry there's been an error, we'll restart")
